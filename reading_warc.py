278cfa1d943563b57f74ce2d1670b259653e2ec9
Tarun Yerneni <tarunyerneni@gmail.com>
2020-09-18 13:48:49 +1000

    Extracting the attributes of the metatags.

--- reading_warc.py
+++ reading_warc.py
@@ -1,6 +1,7 @@
 from warcio.archiveiterator import ArchiveIterator
-from zipfile import ZipFile
-import ujson as json
+from bs4 import BeautifulSoup
+from html.parser import HTMLParser
+
 import requests
 import gzip
 
@@ -10,16 +11,32 @@ Required Fields:
 URI, Content-length, IP Address, server, All the meta-content
 """
 
+html_parser = HTMLParser()
+
 with gzip.open('warc.paths.gz') as file_data:
     
     for line in file_data:
-        http_url = 'https://commoncrawl.s3.amazonaws.com/' + line.strip()
+        
+        http_url = 'https://commoncrawl.s3.amazonaws.com/' + line.decode("utf-8").strip()
         resp = requests.get(http_url, stream=True)
 
         for nested_record in ArchiveIterator(resp.raw, arc2warc=True):
             if nested_record.rec_type == 'response':
-                #print(nested_record.rec_headers.get_header('Content-length') + nested_record.rec_headers.get_header('WARC-Target-URI'))
-                print(nested_record.rec_headers)
-                print(nested_record.http_headers)
-                print(nested_record.content_type)
-                print(nested_record.content_stream().read())
+
+                web_uri = nested_record.rec_headers.get_header('WARC-Target-URI')
+                content_length = nested_record.rec_headers.get_header('Content-length')
+                server_ip_address = nested_record.rec_headers.get_header('WARC-IP-Address')
+                server_name = nested_record.http_headers['Server']
+                
+                web_page = BeautifulSoup(nested_record.content_stream().read(), "html.parser")
+                web_page_title = web_page.title.string
+
+                print(web_page.meta)
+                metadata_dict = {}
+                for tag in web_page_metadata:
+                    print(tag.get("property",None))
+                    metadata_dict[tag.get("property",None)] = tag.get("content",None)
+
+                print(metadata_dict)
+                break
+        break
